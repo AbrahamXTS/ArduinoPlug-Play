c0b929336cb2b796238aca0cd330a1ba1c48b909
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2022-12-03 03:45:27 -0600

    Mejorando la legibilidad del código :book:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import LD from "loudness";
 import JF from "johnny-five";
 
 const { keyTap } = RB;
-const { setMuted, setVolume } = LD;
+const { setVolume } = LD;
 const { Board, Button, Sensor } = JF;
 
 const arduino = new Board();
@@ -13,6 +13,8 @@ arduino.on("ready", () => {
 	const prevButton = new Button(8);
 	const pauseButton = new Button(9);
 	const nextButton = new Button(10);
+
+	const potentiometer = new Sensor("A5");
 
 	prevButton.on("down", () => {
 		keyTap("audio_prev");
@@ -29,71 +31,48 @@ arduino.on("ready", () => {
 	let level;
 	let prevLevel = 0;
 
-	const potentiometer = new Sensor("A5");
+	const changeVolume = (volumeLevel) => {
+
+		if (volumeLevel - prevLevel !== 0) {
+			console.log("El nivel de volumen actual es de ", volumeLevel);
+			setVolume(volumeLevel);
+		}
+
+		prevLevel = volumeLevel;
+	};
 
 	potentiometer.on("change", () => {
 
 		const { value } = potentiometer;
-		console.log({value});
 
-		if (value === 0) {
+		if (value >= 0 && value <= 5) {
 
 			level = 0;
-
-			if (level - prevLevel !== 0) {
-				setVolume(0);
-				console.log(0);
-			}
-
-			prevLevel = level;
+			changeVolume(level);
 		}
 
-		if (value >= 1 && value <= 255) {
+		if (value >= 6 && value <= 255) {
 
 			level = 25;
-			
-			if (level - prevLevel !== 0) {
-				setVolume(25);
-				console.log(25);
-			}
-
-			prevLevel = level;
+			changeVolume(level);
 		}
 
 		if (value >= 256 && value <= 511) {
 
 			level = 50;
-			
-			if (level - prevLevel !== 0) {
-				setVolume(50);
-				console.log(50);
-			}
-
-			prevLevel = level;
+			changeVolume(level);
 		}
 
 		if (value >= 512 && value <= 767) {
 
 			level = 75;
-			
-			if (level - prevLevel !== 0) {
-				setVolume(75);
-				console.log(75);
-			}
-			
-			prevLevel = level;
+			changeVolume(level);
 		}
 
 		if (value >= 768 && value <= 1023) {
 
 			level = 100;
-			
-			if (level - prevLevel !== 0) {
-				setVolume(100);
-				console.log(100);
-			}
-			
-			prevLevel = level;
+			changeVolume(level);
 		}
 	});
 });
