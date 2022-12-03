90c7dab4842f0213066dd894e58cd2fd6e5a2321
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2022-12-03 03:29:36 -0600

    Listo :D

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import RB from "robotjs";
+import LD from "loudness";
 import JF from "johnny-five";
-import { volume } from "node-audio-windows";
 
 const { keyTap } = RB;
+const { setMuted, setVolume } = LD;
 const { Board, Button, Sensor } = JF;
-const { getVolume, setVolume, isMuted, setMute } = volume;
 
 const arduino = new Board();
 
@@ -13,54 +13,6 @@ arduino.on("ready", () => {
 	const prevButton = new Button(8);
 	const pauseButton = new Button(9);
 	const nextButton = new Button(10);
-
-	const potentiometer = new Sensor("A5");
-
-	potentiometer.on("change", () => {
-
-		const { value } = potentiometer;
-
-		if (value === 0) {
-			console.log(0);
-			setMute(true);
-		} 
-
-		if (value >= 1 && value <= 255) {
-			console.log(25);
-			setVolume(0.25);
-		} 
-
-		if (value >= 256 && value <= 511) {
-			console.log(50);
-			setVolume(0.50);
-		} 
-
-		if (value >= 512 && value <= 767) {
-			console.log(75);
-			setVolume(0.75);
-		} 
-
-		if (value >= 768 && value <= 1023) {
-			console.log(100);
-			setVolume(1);
-		}
-	});
-
-	// volDownButton.on("press", () => {
-	// 	keyTap("audio_vol_down");
-	// });
-
-	// volDownButton.on("hold", () => {
-	// 	keyTap("audio_vol_down");
-	// });
-
-	// volUpButton.on("press", () => {
-	// 	keyTap("audio_vol_up");
-	// });
-
-	// volUpButton.on("hold", () => {
-	// 	keyTap("audio_vol_up");
-	// });
 
 	prevButton.on("down", () => {
 		keyTap("audio_prev");
@@ -72,5 +24,76 @@ arduino.on("ready", () => {
 
 	nextButton.on("down", () => {
 		keyTap("audio_next");
+	});
+
+	let level;
+	let prevLevel = 0;
+
+	const potentiometer = new Sensor("A5");
+
+	potentiometer.on("change", () => {
+
+		const { value } = potentiometer;
+		console.log({value});
+
+		if (value === 0) {
+
+			level = 0;
+
+			if (level - prevLevel !== 0) {
+				setVolume(0);
+				console.log(0);
+			}
+
+			prevLevel = level;
+		}
+
+		if (value >= 1 && value <= 255) {
+
+			level = 25;
+			
+			if (level - prevLevel !== 0) {
+				setVolume(25);
+				console.log(25);
+			}
+
+			prevLevel = level;
+		}
+
+		if (value >= 256 && value <= 511) {
+
+			level = 50;
+			
+			if (level - prevLevel !== 0) {
+				setVolume(50);
+				console.log(50);
+			}
+
+			prevLevel = level;
+		}
+
+		if (value >= 512 && value <= 767) {
+
+			level = 75;
+			
+			if (level - prevLevel !== 0) {
+				setVolume(75);
+				console.log(75);
+			}
+			
+			prevLevel = level;
+		}
+
+		if (value >= 768 && value <= 1023) {
+
+			level = 100;
+			
+			if (level - prevLevel !== 0) {
+				setVolume(100);
+				console.log(100);
+			}
+			
+			prevLevel = level;
+		}
 	});
 });
